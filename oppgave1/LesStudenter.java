195752a81ccdf5ee17cba58b557386ad45efa11e
digitnow <46672525+digitnow@users.noreply.github.com>
2026-01-06 18:17:45 +0100

    Update LesStudenter.java
    instruksjoner for LesStudenter

--- oppgave1/LesStudenter.java
+++ oppgave1/LesStudenter.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 // 1. Definere en klasse for data fra en datafil om studenter basert på strukturen i data (se oppgavetekst i README.md)
+// Velg egnede typer for felt i filen med studentinfo, f.eks. første felt kan være en int, siden den representerer id
+// eller studentnummer. Andre felt, som representerer navn til studenten, kan være en String, for eksempel, osv.
 class Student {
-    // Skriv kode her
+    // Skriv kode her ...
 
     // Metoden erstatter en eksisterende metode toString() 
     @Override
@@ -16,17 +18,31 @@ class Student {
 }
 
 // 2. Bruk try-with-resources for sikker filbehandling og les inn data fra fil i en Java datastruktur 
-// ved bruk av Scanner, linje for linje, og til slutt skriv ut datafeltene til System.out
+// ved bruk av Scanner, linje for linje, og til slutt skriv ut datafeltene til 
+// stdout(System.out i Java)
 public class LesStudenter {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(new File(args[0]))) {
-            while (scanner.hasNext()) {
-                // Bruk scanner.nextLine() for allokere Student-objekt for hver linje i students.csv
-                // og skriv ut dataene med den overskrevede toString() metoding i klassen Student
-                // ...
+        try (Scanner leser = new Scanner(new File(args[0]))) {
+            // Tips: bruk while-løkke med leser.hasNextLine() for å sekvensielt gå over alle rader i filen med studentinfo
+            while (leser.hasNext()) {
+                // Tips: bruk leser.nextLine().split(",") for å splitte opp linje (post) i felt (leses inn i en String[])
+                // Tips: sjekk at lengden til post er 3 (du trenger ikke å implementer else) 
+                // Tips: ta inn det første feltet som int (forutsatt at datafeltet i klassen Student er int)
+                // Skriv kode her ...
+                // Tips: alloker Student-objekt for hver linje i filen med studentinfo (fullfør initialisering av objektet Student)
+                Student s = new Student(...);
+                // Tips: Skriv ut dataene med den overskrevede toString() metoden i klassen Student.
+                System.out.println(s);
             }
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
         }
     }
 }
+
+/** Output skal være: 
+Student{id=101, navn='Mickey', program='CS'}
+Student{id=102, navn='Daffy', program='EE'}
+Student{id=103, navn='Donald', program='CS'}
+Student{id=104, navn='Minnie', program='PSY'}
+*/
